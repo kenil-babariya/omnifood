9c5268eb76bae13f134ac62eb5b702ae5bfd77cd
Kenil Babariya <kenil@furrisic.com>
2026-09-23 11:52:20 +0530

    [Added] Make navbar for Mobile

--- assets/script/script.js
+++ assets/script/script.js
@@ -22,3 +22,11 @@ const obs = new IntersectionObserver(
 );
 
 obs.observe(sectionHeroEl);
+
+
+const btnNavEl = document.querySelector(".menu-btn");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
